f79c88f03d0047e64051d353e0500b7502446832
Javier Jaramillo <jjaramillo34@gmail.com>
2023-10-10 15:53:35 -0400

    [chore]: update app logic for ventas

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from streamlit_option_menu import option_menu
 from database import *
 
+
 # set page config and initial setup
 st.set_page_config(page_title='Brakes YES', page_icon='🚀', layout='wide', initial_sidebar_state='auto')
 
@@ -82,6 +83,26 @@ def main():
         st.write(unique_clients[['Client Name', 'Client Email']])
         
 def inicio():
+    st.markdown("""
+        <style>
+        div[data-testid="metric-container"] {
+        background-color: rgba(28, 131, 225, 0.1);
+        border: 1px solid rgba(28, 131, 225, 0.1);
+        padding: 5% 5% 5% 10%;
+        border-radius: 5px;
+        color: rgb(30, 103, 119);
+        overflow-wrap: break-word;
+        }
+
+        /* breakline for metric text         */
+        div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
+        overflow-wrap: break-word;
+        white-space: break-spaces;
+        color: red;
+        }
+        </style>
+        """
+        , unsafe_allow_html=True)
     st.title("Inicio de la aplicacion de ventas")
     st.subheader("Bienvenido a la aplicacion de ventas")
     cols = st.columns(4)
@@ -92,6 +113,8 @@ def inicio():
     # count productos
     count_clientes = get_clientes()
     cols[1].metric(label="Productos", value=len(list(count_clientes)))
+    
+    
     
 def productos():
     st.title("Inventario de Todos los productos")
@@ -113,7 +136,7 @@ def productos():
             input_aux3_1 = st.text_input("Aux3")
         with cols[1]:
             input_stock = st.text_input("Stock")
-            input_precio = st.text_input("Precio")
+            input_precio = st.text_input("Precio", value="0.00")
             
             # format prices 2 decimals
             precio_iva = float(input_precio) * 1.12
@@ -134,7 +157,6 @@ def productos():
                 input_precio_taller = st.text_input("Precio Taller", format(precio_taller, '.2f'), disabled=True)
                 input_ganancia2 = st.text_input("Ganancia 2", value=format(ganancia2, '.2f'), disabled=True)
 
-            
     elif options == 'Actualizar Producto':
         productos = get_productos()
         productos_a = []
@@ -224,28 +246,33 @@ def ventas():
     st.subheader("Bienvenido a la seccion de ventas")
     cliente = get_clientes()
     
+    productos = get_productos()
+    sales = []
+    doc_sales = {}
     # array de productos
-    sales = [
-        {"producto": "Producto 1","nombre": "Name of producto 1", "precio": 10, "cantidad": 1},
-        {"producto": "Producto 2","nombre": "Name of producto 2", "precio": 11, "cantidad": 2},
-        {"producto": "Producto 3","nombre": "Name of producto 3", "precio": 12, "cantidad": 3},
-        {"producto": "Producto 4","nombre": "Name of producto 4", "precio": 13, "cantidad": 4},
-        {"producto": "Producto 5","nombre": "Name of producto 5", "precio": 14, "cantidad": 5},
-        {"producto": "Producto 6","nombre": "Name of producto 6", "precio": 15, "cantidad": 6},
-        {"producto": "Producto 7","nombre": "Name of producto 7", "precio": 16, "cantidad": 7},
-        {"producto": "Producto 8","nombre": "Name of producto 8", "precio": 17, "cantidad": 8},
-        {"producto": "Producto 9","nombre": "Name of producto 9", "precio": 18, "cantidad": 9},
-        {"producto": "Producto 10","nombre": "Name of producto 10", "precio": 19, "cantidad": 10}
-    ]
+    
+    for p in productos:
+        doc_sales['producto'] = p['codigo']
+        doc_sales['nombre'] = p['aplicacion']
+        doc_sales['precio'] = float(p['precio'])
+        doc_sales['cantidad'] = p['stock']
+        if int(p['stock']) > 0:
+            sales.append(doc_sales)
+            doc_sales = {}
+        
+    #st.write(sales)
+        
     # array de productos
     for c in cliente:
         cliente_a.append(c['cedula'])
         clientes_doc[c['cedula']] = c
-        
+    st.subheader("Datos del cliente")
+    cliente_seleccionado = st.multiselect('Selecciona un cliente', cliente_a, default=cliente_a[0])
+    cliente_descuento = st.sidebar.checkbox('Aplicar descuento')
+    cliente_taller_almacen = st.sidebar.radio('Tipo de cliente', ('Taller', 'Almacen'))
     cols = st.columns(2)
+    
     with cols[0]:
-        st.subheader("Datos del cliente")
-        cliente_seleccionado = st.multiselect('Selecciona un cliente', cliente_a, default=cliente_a[0])
         if cliente_seleccionado:
             nombre = clientes_doc[cliente_seleccionado[0]]['nombre']
             apellido = clientes_doc[cliente_seleccionado[0]]['apellido']
@@ -254,97 +281,121 @@ def ventas():
             input_nombre = st.text_input('Nombre', value=nombre)
             input_apellido = st.text_input('Apellido', value=apellido)
             input_cedula = st.text_input('Cedula', value=cedula)
-            input_rating = st.number_input('Rating', min_value=0.0, max_value=5.0, step=0.1)
+            
     with cols[1]:
+        telefono = clientes_doc[cliente_seleccionado[0]]['telefono']
+        direcion = clientes_doc[cliente_seleccionado[0]]['direccion']
+        input_telefono = st.text_input('Telefono', value=telefono)
+        input_direccion = st.text_area('Direccion', value=direcion, height=130)
+    st.write('---')
+    cols = st.columns(1)
+    sales_data = []
+    with cols[0]:
         st.subheader("Datos de la venta")
-        producto = st.multiselect('Selecciona un producto', [s['producto'] for s in sales], default=sales[0]['producto'])
+        producto = st.multiselect('Selecciona un producto', [s['producto'] for s in sales])
+        
+        #productos = get_productos()
+        
+        #st.write('----')
+        #st.write(productos)
+        
+        
         total = 0
-        if len(producto) > 5:
-            num_cols = len(producto) // 5 + 1
+        if len(producto) > 4:
+            num_cols = len(producto) // 4 + 1
             for i in range(num_cols):
-                cols = st.columns(5)
-                for j in range(5):
+                cols = st.columns(4)
+                for j in range(4):
                     if i*5+j < len(producto):
-                        cantidad = cols[j].number_input(f"Cantidad: {producto[i*5+j]}", min_value=0, step=1, key=f"cantidad_{producto[i*5+j]}")
-                        precio = sales[i*5+j]['precio'] * cantidad
-                        precio_iva = precio * 1.12
+                        cantidad = cols[j].number_input(f"{sales[i*4+j]['nombre']}", min_value=0, step=1, key=f"cantidad_{producto[i*4+j]}")
+                        precio = float(sales[i*4+j]['precio']) * cantidad
+                        precio_iva = float(precio) * 1.12
                         # multiplicar por el precio
                         cols[j].write(precio)
                         total += precio_iva
-            st.write(total)
+            st.write(format(total, '.2f'))
                     
         else:
+            cols = st.columns(2)
             for p in range(len(producto)):
-                cantidad = st.number_input(f"Cantidad: {p}", min_value=0, step=1, key=f"cantidad_{p}")
+                with cols[0]:
+                    cantidad = st.number_input(f"No. {p+1} Nombre del producto: {sales[p]['nombre']}", min_value=0, step=1, key=f"cantidad_{producto[p]}")
+                with cols[1]:
+                    descuento = st.number_input(f"Descuento", min_value=0, step=1, key=f"descuento_{producto[p]}")
+                #descuento = st.number_input(f"Descuento", min_value=0, step=1, key=f"descuento_{producto[p]}")
                 precio = sales[p]['precio'] * cantidad
                 precio_iva = precio * 1.12
                 total += precio_iva
-            st.write(total)
-        
+                sales_data.append({
+                    'producto': producto[p],
+                    'cantidad': cantidad,
+                })
+            st.write(format(total, '.2f'))
+            
     st.write('----')
-    if st.button('Submit Sale'):
-        with st.spinner('Submitting sale...'):
+    if st.button('Enviar venta'):
+        with st.spinner('Enviando venta...'):
             time.sleep(2)
-            with st.container():
-                st.markdown(f"# Sumario de la venta")
-                st.markdown(f"### Detalles del cliente")
-                st.markdown(f"- Numero de orden: 1234")
-                st.markdown(f"- Nombre: {input_nombre}")
-                st.markdown(f"- Apellido: {input_apellido}")
-                st.markdown(f"- Cedula: {input_cedula}")
-                st.markdown(f"- Rating: {input_rating}")
-                st.markdown(f"### Detalles de la venta")
-                st.markdown("""| Producto | Cantidad | Precio | Precio con IVA |""")
-                st.markdown("""| -------- | -------- | ------ | -------------- |""")
-                for p in producto:
-                    st.markdown(f"""| {p} | {cantidad} | {precio} | {precio_iva} |""")
-                st.markdown(f"""| **Total**   |        |       | **{round(total)}** |""")
-                text1 = f"""
-                    ## Informacion del cliente
-                    - Nombre: {input_nombre}
-                    - Apellido: {input_apellido}
-                    - Cedula: {input_cedula}
-                    - Rating: {input_rating}
-                    - Numero de orden: 1234
-                """
+            st.success('Venta enviada con exito')
+            #st.write(sales_data)
+            for sale in sales_data:
+                st.write(sale)
+                cantidad_now = sales[p]['cantidad'] - sale['cantidad']
                 
-                text_item = ""
-                
-                for p in producto:
-                    text_item += f"""
-                    ## Informacion del producto
-                    - Producto: {p}
-                    - Cantidad: {cantidad}
-                    - Precio: {precio}
-                    - Precio con IVA: {precio_iva}
-                    """
-                
-                text = f"""
-                    ## Order Details
-                    | Item        | Price |
-                    | ----------- | ----- |
-                    {text_item}
+#            with st.container():
+#                st.markdown(f"# Sumario de la venta")
+#                st.markdown(f"### Detalles del cliente")
+#                st.markdown(f"- Numero de orden: 1234")
+#                st.markdown(f"- Nombre: {input_nombre}")
+#                st.markdown(f"- Apellido: {input_apellido}")
+#                st.markdown(f"- Cedula: {input_cedula}")
+#                st.markdown(f"- Rating: {input_rating}")
+#                st.markdown(f"### Detalles de la venta")
+#                st.markdown("""| Producto | Cantidad | Precio | Precio con IVA |""")
+#                st.markdown("""| -------- | -------- | ------ | -------------- |""")
+#                for p in producto:
+#                    st.markdown(f"""| {p} | {cantidad} | {precio} | {precio_iva} |""")
+#                st.markdown(f"""| **Total**   |        |       | **{round(total)}** |""")
+#                text1 = f"""
+#                    ## Informacion del cliente
+#                    - Nombre: {input_nombre}
+#                    - Apellido: {input_apellido}
+#                    - Cedula: {input_cedula}
+#                    - Rating: {input_rating}
+#                    - Numero de orden: 1234
+#                """
+#                
+#                text_item = ""
+#                
+#                for p in producto:
+#                    text_item += f"""
+#                    ## Informacion del producto
+#                    - Producto: {p}
+#                    - Cantidad: {cantidad}
+#                    - Precio: {precio}
+#                    - Precio con IVA: {precio_iva}
+#                    """
+#                
+#                text = f"""
+#                    ## Order Details
+#                    | Item        | Price |
+#                    | ----------- | ----- |
+#                    {text_item}
+#
+#                    Total: $45
+#
+#                    ## Payment Information
+#                    - Payment Method: Credit Card
+#                    - Card Number: **** **** **** 1234
+#                    - Expiration Date: 05/24
+#                """
+#                markdown_text = text1 + text
+#                st.markdown(f"""
+#                <div style="background-color:#F5F5F5; border-radius:10px; padding: 10px">
+#                {markdown_text}
+#                </div>
+#    """, unsafe_allow_html=True)
 
-                    Total: $45
-
-                    ## Payment Information
-                    - Payment Method: Credit Card
-                    - Card Number: **** **** **** 1234
-                    - Expiration Date: 05/24
-                """
-                markdown_text = text1 + text
-                st.markdown(f"""
-                <div style="background-color:#F5F5F5; border-radius:10px; padding: 10px">
-                {markdown_text}
-                
-                </div>
-                
-                
-                
-    """, unsafe_allow_html=True)
-        
-    
-        
 def home():
     opcion = st.sidebar.radio('Selecciona una opcion', ['Crear Cliente', 'Actualizar Cliente'])
     
